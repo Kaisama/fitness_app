09049e2ea02ca03f390560a0b56529b2af24f6d6
Akshat Bisht <bishtakshat64@gmail.com>
2024-04-04 20:55:21 +0530

    HeroBanner ui done temp

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,6 +1,8 @@
 import { Box , Stack , Typography, Button} from '@mui/material'
 import React from 'react'
 import HeroBannerImage from '../assests/images/banner.png'
+
+
 const HeroBanner = () => {
   return (
    <Box sx={{
@@ -14,14 +16,30 @@ const HeroBanner = () => {
         fontSize:{
             lg:'44px',xs:"40px"
         }
-    }}>
+    }}
+    mb="23px" mt="30px"
+    >
         Sweat,Smile<br /> and Repeat
     </Typography>
-    <Typography fontSize="22px" lineHeight="35px" mb={3}>
+    <Typography fontSize="22px" lineHeight="35px" mb={4}>
         Check out the most effective exercises
     </Typography>
 
-    <Button variant='contained' color='error' href='#exercises'>Explore Exercises</Button>
+    <Typography
+        fontWeight={600}
+        color="#ff2625"
+        sx={{
+            opacity: 0.1,
+            display:{lg:'block',xs:'none'}
+        }}
+        fontSize="200px"
+    >
+        Exercise
+    </Typography>
+
+    <Button variant='contained' color='error' href='#exercises'
+        sx={{backgroundColor:'#ff2625',padding:"10px"}}
+    >Explore Exercises</Button>
     <img src={HeroBannerImage} alt="banner" className="hero-banner-img" />
    </Box>
   )
